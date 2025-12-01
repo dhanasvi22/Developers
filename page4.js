b77cafb89e54019bb28f036d78cdfe7ba6549403
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-12-01 14:41:29 +0530

    js part

---
+++ page4.js
@@ -0,0 +1,16 @@
+// Sidebar toggle button
+const menuBtn = document.getElementById("menu-btn");
+const sidebar = document.querySelector(".sidebar");
+
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+});
+
+const menuItems = document.querySelectorAll(".menu-item");
+
+menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+        document.querySelector(".menu-item.active")?.classList.remove("active");
+        item.classList.add("active");
+    });
+});
